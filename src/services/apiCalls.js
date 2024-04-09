27908903310fcb079a9d7e31651423c28dfa8f9e
Fernando <fernandocatalamunyoz@gmail.com>
2024-04-09 17:41:48 +0200

    feat: creo funcion loginUser en apiCalls.js

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -23,3 +23,31 @@ export const RegisterUser = async (user) => {
     return error;
   }
 };
+
+export const loginUser = async (user) => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  };
+
+  try {
+    const response = await fetch(`${root}auth/login`, options);
+
+    const data = await response.json();
+
+    if (!data.success) {
+      throw new Error(data.message);
+    }
+    //SI NECESITASE TOKEN
+    if (data.message === "Token Error") {
+      dispatch(logout({ credentials: "" }));
+    }
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
