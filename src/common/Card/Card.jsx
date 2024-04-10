40d52bad2e2edee373e8d77275f21b1f131f1a63
Fernando <fernandocatalamunyoz@gmail.com>
2024-04-10 20:35:17 +0200

    feat: añado likes de cada post a falta de funcionalidad del boton de like

--- src/common/Card/Card.jsx
+++ src/common/Card/Card.jsx
@@ -12,9 +12,15 @@ export const Card = ({
       {/* <div>
         <img className="avatarDesign" src={photo} alt={title} />
       </div> */}
-      <div>{title}</div>
-      <div>{description}</div>
-      <div>{likes}</div>
+      <div className="cardTitle">{title}</div>
+      <div className="cardDescription">{description}</div>
+      <div className="cardLikes">{likes}</div>
+      <div className="likeSection">
+        <div className="likeButton" onClick={clickFunction}>
+          Like
+        </div>
+        <div>Likes</div>
+      </div>
     </div>
   );
 };
