08d2831f0492fcf1fdd151dca8cfe5b18aff5166
Fernando <fernandocatalamunyoz@gmail.com>
2024-04-13 14:19:48 +0200

    feat: traigo el post seleccionado a la vista detalle

--- src/pages/DetailPost/DetailPost.jsx
+++ src/pages/DetailPost/DetailPost.jsx
@@ -1,6 +1,30 @@
+import { useSelector } from "react-redux";
 import { detailData } from "../../app/slices/detailSlice";
 import "./DetailPost.css";
+import { Card } from "../../common/Card/Card";
+import { userData } from "../../app/slices/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export const DetailPost = () => {
-  return <div>Soy el detalle</div>;
+  const rdxDetail = useSelector(detailData);
+  const rdxUser = useSelector(userData);
+  const token = rdxUser?.credentials?.token;
+  const navigate = useNavigate();
+  console.log(rdxDetail, "rdxDetail");
+  if (!token) {
+    setTimeout(() => {
+      navigate("/");
+    });
+  }
+  return (
+    <div className="postDetailDesign">
+      <Card
+        title={rdxDetail?.detail?.title}
+        ownerName={rdxDetail?.detail?.ownerName}
+        description={rdxDetail?.detail?.description}
+        likes={rdxDetail?.detail?.likes.length}
+        createdAt={rdxDetail?.detail?.createdAt}
+      />
+    </div>
+  );
 };
